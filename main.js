f093ca6a59f03e25fd2733976607c2f723d75b17
odeebee <bargerybill@gmail.com>
2024-09-16 03:05:51 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+while(true){
+    if(document.getElementById("email").textContent == document.getElementById("emailc").textContent){
+        document.getElementById("eactext").style.color = "green";
+    }
+    else{
+        document.getElementById("eactext").style.color = "red";
+    }
+}
